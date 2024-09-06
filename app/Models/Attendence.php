0cf9879003f501f884272aca026bbb3e1d698037
AhmedMansour88 <125526053+AhmedMansour88@users.noreply.github.com>
2024-09-06 22:36:06 +0300

    attendence->user rel

--- app/Models/Attendence.php
+++ app/Models/Attendence.php
@@ -9,4 +9,7 @@ class AttendenceModel extends Model
 {
     use HasFactory;
     protected $fillable = ['student_id', 'date', 'status'];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
