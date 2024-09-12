b3861f2a1ed7357da7fea9d793d9b0ca683c0a3b
Abdullah Mohamed <bodymohammed201@gmail.com>
2024-09-12 03:58:06 +0300

    create subject and timetable  controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::resource('subjects',SubjectController::class);
 
 require __DIR__.'/auth.php';
