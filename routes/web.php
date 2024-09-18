be9f12a5dcea877d3e04690557ea8df70b1592e6
Abdullah Mohamed <bodymohammed201@gmail.com>
2024-09-18 19:06:57 +0300

    remove routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\AttendenceController;
+
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,7 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('getAttendecesByStudentId/{id}',[AttendenceController::class,'getAttendecesByStudentId']);
 
 
 require __DIR__.'/auth.php';
